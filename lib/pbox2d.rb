cff2acb57918d5234aa78997842354600bfd8e26
monkstone <martin_p@lineone.net>
2015-04-11 08:38:42 +0100

    add more joint stuff

--- lib/pbox2d.rb
+++ lib/pbox2d.rb
@@ -4,27 +4,34 @@ Dir[File.join(working_directory, '*.jar')].each do |jar|
   require jar
 end
 
-# This module allows package access to the wrapped java packages
+# These modules allows package access to the wrapped java packages
 module PB
   include_package 'org.jbox2d.collision.shapes'
   include_package 'org.jbox2d.common'
-  include_package 'org.jbox2d.dynamics'
-  include_package 'org.jbox2d.dynamics.joints'
   include_package 'processing.box2d'
+end
+
+module Dynamics
+  include_package 'org.jbox2d.dynamics'
+  include_package 'org.jbox2d.dynamics.contacts'
+  include_package 'org.jbox2d.dynamics.joints'
 end
 
 ContactListener = Java::OrgJbox2dCallbacks::ContactListener
 Transform = PB::Transform
 Vec2 = PB::Vec2
-Body = PB::Body
-BodyDef = PB::BodyDef
-BodyType = PB::BodyType
-World = PB::World
-Joint = PB::Joint
-JointDef = PB::JointDef
-FixtureDef = PB::FixtureDef
 PolygonShape = PB::PolygonShape
 CircleShape = PB::CircleShape
 ChainShape = PB::ChainShape
+Body = Dynamics::Body
+BodyDef = Dynamics::BodyDef
+BodyType = Dynamics::BodyType
+World = Dynamics::World
+Joint = Dynamics::Joint
+JointDef = Dynamics::JointDef
+DistanceJointDef = Dynamics::DistanceJointDef
+RevoluteJoint = Dynamics::RevoluteJoint
+RevoluteJointDef = Dynamics::RevoluteJointDef
+FixtureDef = Dynamics::FixtureDef
 
 require_relative 'pbox2d/box2d.rb'
