9cbd7646811dc9118ba921b1ec03e12eb2c27714
Martin Prout <monkstone@users.noreply.github.com>
2015-04-17 11:30:50 +0100

    Update dummy_spring.rb

--- examples/mouse_joint/dummy_spring.rb
+++ examples/mouse_joint/dummy_spring.rb
@@ -5,15 +5,14 @@
 # This class avoids test for nil
 class DummySpring
   def initialize; end
-
-  # If it exists we set its target to the mouse location
   def update(_x, _y); end
-
   def display; end
-
   # This is the key function where
   # we attach the spring to an x,y location
-  # and the Box object's location
+  # and the box object's location
+  # @param x (will be mouse_x)
+  # @param y (will be mouse_y)
+  # @return new bound spring
   def bind(x, y, box)
     spring = Spring.new
     spring.bind(x, y, box)
