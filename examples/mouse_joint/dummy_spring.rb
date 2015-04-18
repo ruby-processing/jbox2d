747dc7d2ed6a4aa950905553be2351c1f7be403c
Martin Prout <monkstone@users.noreply.github.com>
2015-04-18 06:29:07 +0100

    Update dummy_spring.rb

--- examples/mouse_joint/dummy_spring.rb
+++ examples/mouse_joint/dummy_spring.rb
@@ -1,6 +1,7 @@
 # dummy_spring.rb by Martin Prout 
+# An example of duck-typing in ruby-processing
 
-# This class avoids test for nil
+# This class avoids test for nil of the vanilla processing version
 class DummySpring
   def initialize; end
   def update(_x, _y); end
